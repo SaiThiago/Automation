e51708f5d3ffe16665af3f3c093f6a52184084e9
saith <saith@SAI>
2022-11-11 11:07:08 +0530

    Editing some code and selecting arrival and departure location.

--- Project/src/Automation/OneWayBooking.java
+++ Project/src/Automation/OneWayBooking.java
@@ -1,6 +1,7 @@
 package Automation;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,24 +10,34 @@ import org.openqa.selenium.interactions.Actions;
 public class OneWayBooking {
 
 	public static void main(String[] args) throws InterruptedException {
-		String departureString = "24 Oct 2022";
-		String dep_month = "October";
+		//String departureString = "24 Oct 2022";
+		String dep_month = "December";
 		String dep_year = "2022";
 		String dep_date = "24";
-		//System.out.println(LocalDate.now());
-		//String datevalue = LocalDate.now().plusDays(3).toString();
-		//System.out.println(datevalue);
+
 		System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		Actions actions = new Actions(driver);
 		driver.manage().window().maximize();
 		driver.get("https://www.goindigo.in/");
 		Thread.sleep(2000);
-		//WebElement depart = driver.findElement(By.xpath("//*[@name=\"or-depart\"]"));
+		
+		//SELECTING DEPARTURE LOCATION AND THE ARRIVAL LOCATION
+		WebElement departLocation = driver.findElement(By.xpath("//*[@id=\"bookFlightTab\"]/form/div[2]/div[2]/div[1]/div/div[1]/div/div/input"));
+		departLocation.click();
+		departLocation.sendKeys(Keys.BACK_SPACE);
+		Thread.sleep(3000);
+		departLocation.sendKeys("Chennai");
+		departLocation.sendKeys(Keys.ENTER);
+		
+		WebElement arrLocation = driver.findElement(By.xpath("//*[@id=\"bookFlightTab\"]/form/div[2]/div[2]/div[1]/div/div[2]/div/div/input"));
+		arrLocation.sendKeys("Bengaluru");
+		arrLocation.sendKeys(Keys.ENTER);
+		
+		
 		WebElement depart = driver.findElement(By.xpath("//*[@id=\"bookFlightTab\"]/form/div[2]/div[2]/div[2]/div/div[1]/div/div[1]/div/input"));
 		actions.moveToElement(depart).click().build().perform();
 		actions.moveToElement(depart).click().build().perform();
-		//depart.sendKeys(departureString);
 
 		while(true) {
 			String month = driver.findElement(By.xpath("//*[@class='ui-datepicker-header ui-widget-header ui-helper-clearfix ui-corner-left']/div/span[1]")).getText();
@@ -45,6 +56,12 @@ public class OneWayBooking {
 			}
 		    
 		}
+		
+		WebElement proceedButton = driver.findElement(By.xpath("//*[@id=\"bookFlightTab\"]/form/div[2]/div[2]/div[2]/div/div[3]/button"));
+		proceedButton.click();
+		
+		
+				
 	}
 
 }
